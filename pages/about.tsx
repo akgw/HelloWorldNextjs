f9ae90844ff4982eff7b4470e5b882a0d0484551
ryunosuke akagawa <ryu.akagawa@gmail.com>
2018-10-12 07:25:36 +0900

    add master tslintの設定追加

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 const About = () => (
   <div>
-    <Link href={"/"}>
+    <Link href={'/'}>
       <a>Index Page</a>
     </Link>
-    <p>This is  the about page</p>
+    <p>This is the about page</p>
   </div>
 );
 
-export default About
+export default About;
