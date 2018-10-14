d698dc2bc438853afa8e60e2fa12e28c3420f033
ryunosuke akagawa <ryu.akagawa@gmail.com>
2018-10-15 08:15:30 +0900

    modify master srcディレクトリを作成

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ https://student-engineer.net/next-js/
 https://qiita.com/udon242/items/ffe8734e325fe3da10d6
 https://www.npmjs.com/package/@zeit/next-typescript
 
+# src directoryの設定
+https://github.com/akgw/HelloWorldNextjs/commit/7a17bb8bb38df134e452816046ba5c77e6354b2b
+
 
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 module.exports = withTypescript({
   webpack(config, options) {
     if (options.isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+      config.plugins.push(new ForkTsCheckerWebpackPlugin({tsconfig: '../tsconfig.json'}));
     }
 
     return config;
